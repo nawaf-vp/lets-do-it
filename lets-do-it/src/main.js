584cacc29bf831d392ad6b3e2ae879f25d96f249
Nawaf v p <nawafvp.dev@gmail.com>
2023-08-24 21:31:23 +0530

    added vuex to main component

--- lets-do-it/src/main.js
+++ lets-do-it/src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import './style.css'
 import App from './App.vue'
 import { todoStore } from "./store/toDoListStore";
 
-createApp(App).mount('#app')
+new Vue({
+    store: todoStore,
+    render: (h) => h(App),
+  }).$mount("#app");
